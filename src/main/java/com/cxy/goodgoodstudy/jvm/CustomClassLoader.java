2497ed8c5ecc19c7fde258796c35cebcc8f4fe77
wuhaigang <wuhg1127@163.com>
2021-02-09 17:59:15 +0800

    打破类加载的双亲委派机制

--- src/main/java/com/cxy/goodgoodstudy/jvm/CustomClassLoader.java
+++ src/main/java/com/cxy/goodgoodstudy/jvm/CustomClassLoader.java
@@ -13,6 +13,11 @@ public class CustomClassLoader {
             this.classPath = classPath;
         }
 
+        /**
+         * 因为加载的时候是使用的loadClass，所以里面核心调用了findClass，我们只需要重写这个方法就能实现自定义类加载器
+         * @param name 包名称
+         * @return 加载完成的Class
+         */
         @Override
         protected Class<?> findClass(String name) {
             try {
@@ -24,6 +29,41 @@ public class CustomClassLoader {
             return findClass(name);
         }
 
+        /**
+         * 打破双亲委派，需要重写loadClass方法，因为这个方法核心会调用parent的类加载器
+         * 但是由于Object类，或者类中使用的其他类，都是需要在我们自定义类加载器加载的时候跟着加载
+         * 所以我们只加载我们的包路径类，其他的还是交给parent的类加载器进行加载
+         * @param name 包路径名称
+         * @param resolve 是否需要解析
+         * @return Class对象
+         * @throws ClassNotFoundException 找不到类异常
+         */
+        @Override
+        protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+            synchronized (getClassLoadingLock(name)) {
+                // First, check if the class has already been loaded
+                Class<?> c = findLoadedClass(name);
+                long t0 = System.nanoTime();
+                if (c == null) {
+                    // If still not found, then invoke findClass in order
+                    // to find the class.
+                    long t1 = System.nanoTime();
+                    if (!name.startsWith("com.cxy.goodgoodstudy.jvm")) {
+                        c = this.getParent().loadClass(name);
+                    } else {
+                        c = findClass(name);
+                    }
+                    // this is the defining class loader; record the stats
+                    sun.misc.PerfCounter.getParentDelegationTime().addTime(t1 - t0);
+                    sun.misc.PerfCounter.getFindClassTime().addElapsedTimeFrom(t1);
+                    sun.misc.PerfCounter.getFindClasses().increment();
+                }
+                if (resolve) {
+                    resolveClass(c);
+                }
+                return c;
+            }
+        }
 
         private byte[] loadByte(String name) throws Exception {
             name = name.replaceAll("\\.", "/");
---
+++ src/main/java/com/cxy/goodgoodstudy/jvm/User1.java
@@ -0,0 +1,8 @@
+package com.cxy.goodgoodstudy.jvm;
+
+public class User1 {
+
+    public void sayHello() {
+        System.out.println("hello world");
+    }
+}
